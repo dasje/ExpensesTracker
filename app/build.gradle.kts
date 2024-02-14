051caecf493ec10fbbc8ec1e2f6136f33ec6da95
Ben <kwalijkje@live.com>
2024-02-14 17:15:42 +0100

    Add datastore dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,4 +46,5 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("com.google.android.material:material:1.12.0-alpha03")
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
 }
