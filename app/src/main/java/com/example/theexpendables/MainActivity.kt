5b12ca6444ee86d567c4974558539e21a532196c
Ben <kwalijkje@live.com>
2024-03-27 10:00:10 +0100

    init

--- app/src/main/java/com/example/theexpendables/MainActivity.kt
+++ app/src/main/java/com/example/theexpendables/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.theexpendables
 
 import android.os.Bundle
+import android.util.Log
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.LinearLayout
+import android.widget.PopupWindow
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,5 +36,38 @@ class MainActivity : AppCompatActivity() {
 
         val adapter = ExpenseAdapter(data)
         expenseRecycler.adapter = adapter
+
+        val newExpenseButton = findViewById<ImageButton>(R.id.addExpenseButton)
+        newExpenseButton?.setOnClickListener {
+            Toast.makeText(
+                this@MainActivity,
+                "Adding a new expense",
+                Toast.LENGTH_SHORT
+            ).show()
+            onButtonShowPopupWindowClick(findViewById(R.id.main))
+        }
+    }
+
+    private fun onButtonShowPopupWindowClick(view: View?) {
+
+        // inflate the layout of the popup window
+        val inflater = getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val popupView: View = inflater.inflate(R.layout.new_expense, null)
+
+        // create the popup window
+        val width = LinearLayout.LayoutParams.WRAP_CONTENT
+        val height = LinearLayout.LayoutParams.WRAP_CONTENT
+        val focusable = true // lets taps outside the popup also dismiss it
+        val popupWindow = PopupWindow(popupView, width, height, focusable)
+
+        // show the popup window
+        // which view you pass in doesn't matter, it is only used for the window tolken
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0)
+
+        // dismiss the popup window when touched
+        popupView.setOnTouchListener { v, event ->
+            popupWindow.dismiss()
+            true
+        }
     }
 }
