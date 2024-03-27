bc62c7c96e18100029e88f3220246765ededf473
Ben <kwalijkje@live.com>
2024-03-27 08:45:15 +0100

    Add expense recycler

---
+++ app/src/main/java/com/example/theexpendables/ExpenseAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.theexpendables
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ExpenseAdapter (private val mList: List<ExpenseDataType>) : RecyclerView.Adapter<ExpenseAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.single_expense, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    // binds the list items to a view
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        val itemsViewModel = mList[position]
+        // sets the text to the textview from our itemHolder class
+        holder.textView.text = itemsViewModel.name
+
+    }
+
+    // return the number of the items in the list
+    override fun getItemCount(): Int {
+        return mList.size
+    }
+
+    // Holds the views for adding it to image and text
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val textView: TextView = itemView.findViewById(R.id.textExpenseListName)
+    }
+}
