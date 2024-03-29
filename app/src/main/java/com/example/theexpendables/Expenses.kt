6373d26a09ddf2cca5f7775c5919195919bfddfb
Ben <kwalijkje@live.com>
2024-03-29 15:37:37 +0100

    Logic to calculate total active expenses

--- app/src/main/java/com/example/theexpendables/Expenses.kt
+++ app/src/main/java/com/example/theexpendables/Expenses.kt
@@ -20,5 +20,21 @@ class Expenses {
             }
             return true
         }
+
+        fun totalActiveExpenses(
+            expenses: ArrayList<ExpenseDataType>
+        ): Pair<Float, Float> {
+            var totalExpenses = 0.0
+            var totalRemainingExpenses = 0.0
+            for (expense in expenses) {
+                if (expense.active) {
+                    totalExpenses += expense.value
+                    if (!expense.paid) {
+                        totalRemainingExpenses += expense.value
+                    }
+                }
+            }
+            return Pair(totalExpenses.toFloat(), totalRemainingExpenses.toFloat())
+        }
     }
 }
